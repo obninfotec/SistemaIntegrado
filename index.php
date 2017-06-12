1dd1efffa849e7747d543172aaec887f62d4d052
Osvaldo <Osvaldo@192.168.0.133>
2017-06-12 13:18:55 -0300

    Informações deste Sistema

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ require 'vendor/autoload.php';
 $app = new DRouter\App();
 
 $app->get('/', function(){
-    echo 'Estou na Home';
+    echo 'Estou aqui, na Home';
 });
 
-$app->get('/contato', function(){
+$app->get('/contato/', function(){
     echo '<form action="" method="post">';
     echo '<input type="text" name="titulo" placeholder="titulo" /><br />';
     echo '<input type="text" name="valor" placeholder="valor" /><br />';
