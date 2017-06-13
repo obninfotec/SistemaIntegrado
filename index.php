9401a1f5de21761d916ead8b7a7651335871e7a6
Osvaldo <Osvaldo@192.168.0.133>
2017-06-13 01:24:15 -0300

    Arquivo Index Base

--- index.php
+++ index.php
@@ -9,11 +9,29 @@ require 'vendor/autoload.php';
 
 $app = new DRouter\App();
 
+// daqui
+$container = $app->getContainer();
+// var_dump($container);
+$container->pdo = $container->shared(function(){
+    return new PDO('mysql:host=localhost;dbname=shop', 'root', '');
+});
+
+$container->joao = $container->shared(function(){
+    $outracoisa = new OutraCoisa();
+    return new helper($outracoisa);
+});
+
+$container->dependencia = $container->shared(function() use ($container){
+    return new Dependencia($container->pdo);
+});
+// ate aqui
 $app->get('/', function(){
+    var_dump($this->pdo);
     echo 'Estou aqui, na Home';
 });
 
 $app->get('/contato', function(){
+    var_dump($this->pdo);
     echo '<form action="" method="post">';
     echo '<input type="text" name="titulo" placeholder="titulo" /><br />';
     echo '<input type="text" name="valor" placeholder="valor" /><br />';
@@ -22,10 +40,17 @@ $app->get('/contato', function(){
 });
 
 $app->post('/contato', function(){
-    $request = $_POST;
-    print_r($request);
+    // $request = $_POST;
+    // print_r($request);
+    $request = $this->request->getParsedBody();
+    var_dump($request);
 });
 
+$app->delete('/contato', function(){
+    echo "Tipo DELETE";
+    $request = $this->request->getParsedBody();
+    var_dump($request);
+});
 $app->run();
 
 
