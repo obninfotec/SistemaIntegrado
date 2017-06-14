a832d550b22d0ff0237184c6d46b246f1e3f15c3
Osvaldo <Osvaldo@192.168.0.133>
2017-06-14 17:57:32 -0300

    Arquivo de Index novo

--- index.php
+++ index.php
@@ -9,9 +9,21 @@ require 'vendor/autoload.php';
 
 $app = new DRouter\App();
 
+$container = $app->getContainer();
+
+$container->pdo = $container->shared(function(){
+    return new PDO('mysql:host=localhost;dbname=videoaulas', 'root', '');
+});
+
 $app->get('/single/:slug', function($slug){
     echo 'Estou na página single do post <br />';
     echo 'O Slug é : <b>'.$slug.'</b>';
+});
+
+$app->get('/:categoria/:subcategoria', function($categoria, $subcategoria){
+    echo 'Estou na subcategoria<br />';
+    echo 'Categoria: <b>'.$categoria.'</b><br />';
+    echo 'Subcategoria: <b>'.$subcategoria.'</b><br />';
 });
 
 $app->run();
