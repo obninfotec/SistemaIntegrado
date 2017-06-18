e86a19e3252a927813f8c1ca2141796c85c33642
Osvaldo <Osvaldo@192.168.0.133>
2017-06-18 12:27:28 -0300

    Arquivo Index Atual

--- index.php
+++ index.php
@@ -7,7 +7,15 @@
 header('Content-Type: text/html; charset=utf-8');
 require 'vendor/autoload.php';
 
+class Helper {
+    public function teste(){
+	echo 'deu-certo';
+    }
+}
+
 $app = new DRouter\App();
+$app->render->setViewsFolder(__DIR__.'/Views/');
+$app->render->setHf('header.php', 'footer.php');
 
 $container = $app->getContainer();
 
@@ -15,5 +23,38 @@ $container->pdo = $container->shared(function(){
     return new PDO('mysql:host=localhost;dbname=videoaula', 'root', '');
 });
 
+$container->helper = $container->shared(function(){
+    return new Helper();
+});
+
+$app->render->setAsGlobal([
+    'helper' => $container->helper
+]);
+
+$app->get('/', function(){
+    // echo 'Estou na HOME';
+    $this->render->load('home.php', [
+	'teste' => 'Frase de teste'
+    ],
+	false
+    );
+});
+
+$app->group('/admin', function() use($app){
+    $app->get('/cadastrar-post', function(){
+	echo 'Estou no admin/cadastrar-post';
+	$this->render->load('cadastrar-post.php', []);
+    });
+    $app->get('/cadastrar-usuario', function(){
+	echo 'Estou na página admin/cadastrar-usuario';
+    });
+});
+
+$app->group('/cliente', function() use($app){
+    $app->get('/meus-pedidos', function(){
+	echo '/clientes/meus-pedidos';
+    });
+    
+});
 $app->run();
 
