2fdf5416bc72372722e6854963ac3b758da97ef9
Osvaldo <Osvaldo@192.168.0.133>
2017-06-18 12:19:55 -0300

    Arquivo base de testes HOME

--- Views/home.php
+++ Views/home.php
@@ -1,6 +1,8 @@
 
 <h1>Estou na Home</h1>
+<b><?php echo $teste; ?></b>
+
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 Aperiam est cum ab iusto ut sit dolores distinctio impedit tenetur nostrum,
 quisquam aspernatur fuga eius ex aut. Nemo natus, reiciendis ab.</p>
-    
+<?php $helper->teste(); ?>
