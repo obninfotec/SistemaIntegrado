4ef827a74c6a869e69e4b40ff577fa9944a7a4b0
Osvaldo <Osvaldo@192.168.0.133>
2017-06-18 10:59:22 -0300

    Apenas um teste View (HOME)

---
+++ Views/home.php
@@ -0,0 +1,6 @@
+
+<h1>Estou na Home</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+Aperiam est cum ab iusto ut sit dolores distinctio impedit tenetur nostrum,
+quisquam aspernatur fuga eius ex aut. Nemo natus, reiciendis ab.</p>
+    
